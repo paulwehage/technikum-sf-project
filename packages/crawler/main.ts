ba27716c86d310558cf5a1487c0234e12b2ca50e
Benedict Allerberger <benedict.allerberger@bytesource.net>
2023-03-15 11:39:51 +0100

    Fix registering of schema

--- packages/crawler/main.ts
+++ packages/crawler/main.ts
@@ -1,18 +1,30 @@
 import { Kafka } from "kafkajs";
 import { SchemaRegistry, SchemaType } from "@kafkajs/confluent-schema-registry";
-import { ShopMessage, ShopMessageSchema } from "schema";
 
-const registry = new SchemaRegistry({ host: "http://registry:8081/" });
+const registry = new SchemaRegistry({ host: "http://localhost:8081" });
 const kafka = new Kafka({
   brokers: ["localhost:9092"],
 });
 
 const producer = kafka.producer();
 
+const ShopMessageSchema = {
+  type: "record",
+  name: "ShopMessage",
+  namespace: "examples",
+  fields: [
+    { name: "shopName", type: "string" },
+    { name: "date", type: { type: "string", logicalType: "date" } },
+    { name: "inStock", type: "boolean" },
+    { name: "price", type: "float" },
+  ],
+};
+
 async function registerSchema() {
+  const schema = JSON.stringify(ShopMessageSchema);
   const { id } = await registry.register({
     type: SchemaType.AVRO,
-    schema: JSON.stringify(ShopMessageSchema),
+    schema,
   });
 
   return id;
