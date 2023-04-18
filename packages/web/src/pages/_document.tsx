db3092c26cc2964edc1198459d820672e4620f18
Benedict Allerberger <benedict.allerberger@bytesource.net>
2023-04-18 23:07:28 +0200

    Remove default components from `web`

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <title>SWF</title>
+      </Head>
+
+      <body className="bg-stone-50">
         <Main />
         <NextScript />
       </body>
